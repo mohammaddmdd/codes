52a6b82abd7bf80cc19eebda3b3b1d1f6f3f723b
Ho3einMF <hossein.mansouri11@gmail.com>
2023-10-04 13:21:44 +0330

    chore: custom module was added
    The name of the custom module is "arad_jalali_toolkit" which is used for the localization of date and datetime fields and default calendar module

---
+++ custom_addons/arad_jalali_toolkit/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Jalali Calendar",
+    'version': '1',
+    'author': 'Arad Pergas Co',
+    'website': 'https://aradware.ir',
+    "license": "LGPL-3",
+    'summary': 'Odoo Jalali Localization',
+    "description": 'Jalali localization for date and datetime fields and calendar module',
+    "depends": ['base', 'web'],
+    'category': 'Productivity',
+    'sequence': 1,
+    'installable': True,
+    'application': True,
+}
