ce237cccb51beabc57d99158398f64d57295beef
Ho3einMF <hossein.mansouri11@gmail.com>
2023-11-13 15:06:23 +0330

    fix: the wrong display of date and datetime fields in the list view has been fixed
    now all date and datetime fields in list views are forced to display Jalali format instead of Gregorian.

---
+++ custom_addons/arad_jalali_toolkit/static/src/views/persian_list/list_renderer.js
@@ -0,0 +1,30 @@
+/** @odoo-module **/
+
+
+import { onMounted } from "@odoo/owl";
+import { ListRenderer } from "@web/views/list/list_renderer";
+
+import { PersianDateField } from "../fields/persian_date/persian_date_field";
+import { PersianDateTimeField } from "../fields/persian_datetime/persian_datetime_field";
+
+
+export class PersianListRenderer extends ListRenderer {
+    setup() {
+
+        super.setup()
+
+        onMounted(() => {
+            for (const column of this.state.columns) {
+                debugger
+                if (column.FieldComponent === PersianDateField) {
+                    column.widget = 'date'
+                } else if (column.FieldComponent === PersianDateTimeField) {
+                    column.widget = 'datetime'
+                }
+            }
+
+            this.activeElement = this.uiService.activeElement;
+        })
+
+    }
+}
---
+++ custom_addons/arad_jalali_toolkit/static/src/views/persian_list/list_view.js
@@ -0,0 +1,23 @@
+/** @odoo-module */
+
+
+import { registry } from "@web/core/registry";
+import { listView } from "@web/views/list/list_view";
+
+import { PersianListRenderer } from "./list_renderer";
+
+
+export const persianListView = {
+    ...listView,
+    Renderer: PersianListRenderer,
+}
+
+
+// remove default list view
+registry.category("views").remove("list");
+
+// for all default tree tags that have not js_class attribute
+registry.category("views").add("list", persianListView);
+
+// for all tree tags that have their specific js_class attribute
+registry.category("views").add("hr_contract_history_list", persianListView);
