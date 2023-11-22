eae7e6f57498a3e918cd86bc07954ae25a2aefd8
Ho3einMF <hossein.mansouri11@gmail.com>
2023-11-22 15:23:09 +0330

    refactor: anar_point_of_sale module replaced by arad_point_of_sale module
    the module logo changed.

--- custom_addons/arad_point_of_sale/__manifest__.py
+++ custom_addons/arad_point_of_sale/__manifest__.py
@@ -1,14 +1,14 @@
 {
-    "name": "Arad Point of Sale",
+    "name": "Anar Point of Sale",
     'version': '1.0',
     'author': 'Arad Pergas Co',
     'website': 'https://aradware.ir',
     "license": "LGPL-3",
-    'summary': 'Arad Point of Sale custom module',
+    'summary': 'Anar Point of Sale custom module',
     "description": 'Customization for Point of Sale module',
     "depends": ['point_of_sale'],
     'category': 'Sales/Point of Sale',
-    'sequence': 2,
+    'sequence': 1,
     'installable': True,
     'application': True,
 
